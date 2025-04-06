ce005f86a1778e435ed552bd0447c26fce73a372
bacsiemon <johnsieunhan816@gmail.com>
2025-04-06 16:17:22 +0700

    enable swagger in release build

--- server/speezs.API/Program.cs
+++ server/speezs.API/Program.cs
@@ -18,11 +18,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//	app.UseSwagger();
+//	app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(builder =>
 {
